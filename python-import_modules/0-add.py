2ccf67aee339b5f6194ac34b3732434dfef9335b
Ladecodes1 <n.bamgbose@alustudent.com>
2025-02-21 10:40:53 -0800

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# Imports the add function from add_0.py and prints the result of 1 + 2
+
+from add_0 import add
+
 if __name__ == "__main__":
-    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b,)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
