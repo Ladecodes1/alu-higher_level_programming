23379d2ae7a36eafc6a3f8eb626eebaec9c1dd8a
Ladecodes1 <n.bamgbose@alustudent.com>
2025-03-14 11:22:00 -0700

    genre

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- List all TV shows with their corresponding genre_id (if available)
+-- If a show has no genre, display NULL
+-- Results are sorted by tv_shows.title and tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
